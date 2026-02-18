bf8bce0ce3470de930db2b6d05948b19b2f1dfd1
Rahul Shakya <rahulshakya7685@gmail.com>
2026-02-18 22:01:07 +0530

    Some Session - 5 changes

--- Session-05/src/App.tsx
+++ Session-05/src/App.tsx
@@ -1,45 +1,34 @@
-import { useEffect, useState } from "react";
-  // 10 elements in array
-  const items = [
-    "Apple",
-    "Banana",
-    "Mango",
-    "Orange",
-    "Pineapple",
-    "Grapes",
-    "Watermelon",
-    "Strawberry",
-    "Papaya",
-    "Kiwi",
-  ];
+import { useState } from "react";
+
+const items = [
+  "Apple",
+  "Banana",
+  "Mango",
+  "Orange",
+  "Pineapple",
+  "Grapes",
+  "Watermelon",
+  "Strawberry",
+  "Papaya",
+  "Kiwi",
+];
 
 function App() {
+  const [search, setSearch] = useState("");
 
-   const [search, setSearch] = useState("");
-  const [filteredItems, setFilteredItems] = useState<string[]>(items);
-
-  useEffect(() => {
-    const result = items.filter((item) =>
-      item.toLowerCase().includes(search.toLowerCase())
-    );
-
-    setFilteredItems(result);
-  }, [search]);
+  const filteredItems = items.filter((item) =>
+    item.toLowerCase().includes(search.toLowerCase())
+  );
 
   return (
-    <div style={{ padding: "40px", fontFamily: "Arial" }}>
-      <h2>Simple Search Project</h2>
+    <div style={{ padding: "40px" }}>
+      <h2>Search Fruits</h2>
 
       <input
         type="text"
-        placeholder="Search fruit..."
+        placeholder="Search..."
         value={search}
         onChange={(e) => setSearch(e.target.value)}
-        style={{
-          padding: "8px",
-          width: "200px",
-          marginBottom: "20px",
-        }}
       />
 
       <ul>
